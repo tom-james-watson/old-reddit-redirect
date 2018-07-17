dd71d69ecedc4182be019854a68afb5a594ef39f
Lachcim <jajnick@hotmail.com>
2018-07-17 16:21:50 +0200

    Reduce code to single listener

--- background.js
+++ background.js
@@ -2,27 +2,15 @@ const oldReddit = "https://old.reddit.com"
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    return {redirectUrl: oldReddit + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]}
-  },
-  {
-    urls: [
-      "*://reddit.com/*",
-      "*://www.reddit.com/*",
-      "*://np.reddit.com/*",
-      "*://new.reddit.com/*",
-      "*://pay.reddit.com/*"
-    ],
-    types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-  },
-  ["blocking"]
-)
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
     var urlParser = document.createElement("a");
     urlParser.href = details.url;
-    var pathname = urlParser.pathname;
+    
+    //redirect to old.reddit.com
+    if (urlParser.hostname != "old.reddit.com")
+      return {redirectUrl: oldReddit + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
     
     //redirect /user/* and /user/*/ to /user/*/overview
+    var pathname = urlParser.pathname;
     if (pathname.match(/\/user\/[\w-]+\/?$/)) {
       var tail = "overview";
       if (pathname.lastIndexOf("/") != pathname.length - 1) tail = "/" + tail;
@@ -32,12 +20,8 @@ chrome.webRequest.onBeforeRequest.addListener(
   },
   {
     urls: [
-      "*://reddit.com/user/*",
-      "*://old.reddit.com/user/*",
-      "*://www.reddit.com/user/*",
-      "*://np.reddit.com/user/*",
-      "*://new.reddit.com/user/*",
-      "*://pay.reddit.com/user/*"
+      "*://reddit.com/*",
+      "*://*.reddit.com/*"
     ],
     types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
   },

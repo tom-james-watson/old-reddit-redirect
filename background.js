fb325a7ae4619f564678b14004545bbdff9caca9
Zamicol <zamicol@gmail.com>
2018-11-19 23:59:07 -0700

    add support for poll pages

--- background.js
+++ background.js
@@ -2,6 +2,12 @@ const oldReddit = "https://old.reddit.com"
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
+
+    // Exclude poll pages
+    if (details.url.match(/^https?:\/\/(www.)*reddit.com\/poll/)){
+      return 
+    }
+
     return {redirectUrl: oldReddit + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]}
   },
   {

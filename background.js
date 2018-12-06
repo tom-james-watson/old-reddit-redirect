3824a950587684bec3bae43582b2d7eee5514d52
Tom Watson <tom@tomjwatson.com>
2018-12-06 15:58:41 +0100

    linting fixes

--- background.js
+++ background.js
@@ -2,10 +2,9 @@ const oldReddit = "https://old.reddit.com"
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-
     // Exclude poll pages
-    if (details.url.match(/^https?:\/\/(www.)*reddit.com\/poll/)){
-      return 
+    if (details.url.match(/^https?:\/\/(www.)*reddit.com\/poll/)) {
+      return
     }
 
     return {redirectUrl: oldReddit + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]}

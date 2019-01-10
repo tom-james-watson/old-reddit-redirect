ca31eb5056f270c12addbe9d80e62ae72700a29e
atbanpc <atbanpc@gmail.com>
2019-01-10 21:57:38 +0800

    Add ability to temporarily disable and optimize

--- background.js
+++ background.js
@@ -1,9 +1,16 @@
 const oldReddit = "https://old.reddit.com";
+var disabled = false;
 
+// Redirect requests
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
+    // Check if disabled
+    if(disabled)  {
+      return;
+    }
+
     // Exclude poll pages
-    if (details.url.match(/^https?:\/\/(www\.)*reddit.com\/poll/)) {
+    if (/^https?:\/\/(www\.)*reddit.com\/poll/.test(details.url)) {
       return;
     }
 
@@ -32,3 +39,17 @@ chrome.webRequest.onBeforeRequest.addListener(
   },
   ["blocking"]
 );
+
+// Browser Action
+chrome.browserAction.onClicked.addListener(() => {
+  disabled = !disabled;
+
+  // Set icon and name
+  if(disabled)  {
+    chrome.browserAction.setIcon({"path": {"48": "img/disabledicon48.png", "128": "img/disabledicon128.png"}});
+    chrome.browserAction.setTitle({"title": "Old Reddit Redirect (Disabled)"});
+  } else {
+    chrome.browserAction.setIcon({"path": {"48": "img/icon48.png", "128": "img/icon128.png"}});
+    chrome.browserAction.setTitle({"title": "Old Reddit Redirect (Enabled)"});
+  }
+});

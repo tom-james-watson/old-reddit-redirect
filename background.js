90181d412526e99435fb56dd99f0f456b94f55fc
Michał Szopiński <jajnick@hotmail.com>
2020-07-16 13:31:36 +0200

    Add exception system

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 const oldReddit = "https://old.reddit.com";
+const excludedSubdomains = ["blog.reddit.com", "oauth.reddit.com", "out.reddit.com"];
+const excludedPaths = ["/poll", "/gallery", "/rpan", "/settings"];
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
@@ -6,6 +8,12 @@ chrome.webRequest.onBeforeRequest.addListener(
     urlParser.href = details.url;
     
     if (urlParser.hostname == "old.reddit.com") return;
+    
+    if (excludedSubdomains.indexOf(urlParser.hostname) != -1) return;
+    
+    for (const path of excludedPaths)
+        if (urlParser.pathname.indexOf(path) == 0)
+            return;
     
     return {redirectUrl: oldReddit + urlParser.pathname};
   },

629926126eb1149b5636ace05b59067ece1a433f
Michał Szopiński <jajnick@hotmail.com>
2020-07-17 15:19:05 +0200

    Remove unnecessary exceptions

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
 const oldReddit = "https://old.reddit.com";
-const excludedSubdomains = [
-  "blog.reddit.com",
-  "mod.reddit.com",
-  "oauth.reddit.com",
-  "out.reddit.com"];
 const excludedPaths = [
-  "/chat",
   "/gallery",
   "/poll",
   "/rpan",
@@ -18,8 +12,6 @@ chrome.webRequest.onBeforeRequest.addListener(
     urlParser.href = details.url;
     
     if (urlParser.hostname == "old.reddit.com") return;
-    
-    if (excludedSubdomains.indexOf(urlParser.hostname) != -1) return;
     
     for (const path of excludedPaths) {
       if (urlParser.pathname.indexOf(path) == 0) return;

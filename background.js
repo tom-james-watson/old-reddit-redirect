850be1dec3dcfd45d6b91cfce1b9d9f05ae56fcc
Tom Watson <tom@tomjwatson.com>
2023-06-17 20:02:52 +0200

    manifest v3 testing

--- background.js
+++ background.js
@@ -9,6 +9,7 @@ const excludedPaths = [
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
+    console.log({ details });
     const url = new URL(details.url);
 
     if (url.hostname === "old.reddit.com") return;
@@ -47,3 +48,45 @@ chrome.webRequest.onBeforeRequest.addListener(
   },
   ["blocking"]
 );
+
+console.log("loaded");
+
+// chrome.webRequest.onBeforeSendHeaders.addListener(
+//   function (details) {
+//     console.log(arguments, { details });
+//     // const url = new URL(details.url);
+//
+//     // if (url.hostname !== "preview.redd.it") return;
+//
+//     // for (const path of excludedPaths) {
+//     //   if (url.pathname.indexOf(path) === 0) return;
+//     // }
+//
+//     // if (url.pathname.indexOf("/gallery") === 0) {
+//     //   return { redirectUrl: oldReddit + '/comments' + url.pathname.slice("/gallery".length) };
+//     // }
+//
+//     // return { redirectUrl: oldReddit + url.pathname + url.search + url.hash };
+//   },
+//   {
+//     urls: [
+//       "*://reddit.com/*",
+//       "*://www.reddit.com/*",
+//       "*://np.reddit.com/*",
+//       "*://amp.reddit.com/*",
+//       "*://i.reddit.com/*",
+//       "*://preview.redd.it/*",
+//     ],
+//     types: [
+//       "main_frame",
+//       "sub_frame",
+//       "stylesheet",
+//       "script",
+//       "image",
+//       "object",
+//       "xmlhttprequest",
+//       "other",
+//     ],
+//   },
+//   ["blocking"]
+// );

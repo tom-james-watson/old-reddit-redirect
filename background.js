a201895d1fccf4d486cb12d72a91bac1671cb156
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2024-04-08 16:22:00 +0200

    fix infinite loop when open a RSS or JSON url

--- background.js
+++ background.js
@@ -9,6 +9,8 @@ const excludedPaths = [
   /^\/r\/[a-zA-Z0-9_]+\/s\/.*/, // eg https://reddit.com/r/comics/s/TjDGhcl22d
   /^\/appeals?/,
   /\/r\/.*\/s\//,
+  /\.json$/,
+  /\.rss$/,
 ];
 
 chrome.webRequest.onBeforeRequest.addListener(

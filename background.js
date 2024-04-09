607e6e5829cd78bc210a79b3e55968faff054dd2
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2024-04-09 07:39:09 +0200

    add a menu option choose the style of the redirect

--- background.js
+++ background.js
@@ -1,4 +1,18 @@
-const oldReddit = "https://old.reddit.com";
+styleRedirect = "old"
+const styleRedirectId = "style-reddit-redirect"
+const styleRedirectData = {
+    "old": {
+        "title": "Old classic style",
+        "url": "https://old.reddit.com",
+        "hostname": "old.reddit.com"
+    },
+    "boxy":{
+        "title": "\"New\" boxy style",
+        "url": "https://new.reddit.com",
+        "hostname": "new.reddit.com"
+    }
+}
+
 const excludedPaths = [
   /^\/media/,
   /^\/poll/,
@@ -9,13 +23,15 @@ const excludedPaths = [
   /^\/r\/[a-zA-Z0-9_]+\/s\/.*/, // eg https://reddit.com/r/comics/s/TjDGhcl22d
   /^\/appeals?/,
   /\/r\/.*\/s\//,
+  /\.json$/,
+  /\.rss$/,
 ];
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     const url = new URL(details.url);
 
-    if (url.hostname === "old.reddit.com") return;
+    if (url.hostname === styleRedirectData[styleRedirect]["hostname"]) return;
 
     for (const path of excludedPaths) {
       if (path.test(url.pathname)) return;
@@ -24,11 +40,11 @@ chrome.webRequest.onBeforeRequest.addListener(
     if (url.pathname.indexOf("/gallery") === 0) {
       return {
         redirectUrl:
-          oldReddit + "/comments" + url.pathname.slice("/gallery".length),
+        styleRedirectData[styleRedirect]["url"] + "/comments" + url.pathname.slice("/gallery".length),
       };
     }
 
-    return { redirectUrl: oldReddit + url.pathname + url.search + url.hash };
+    return { redirectUrl: styleRedirectData[styleRedirect]["url"] + url.pathname + url.search + url.hash };
   },
   {
     urls: [
@@ -83,3 +99,25 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
   },
   ["blocking", "requestHeaders"],
 );
+
+chrome.contextMenus.create({
+    id: styleRedirectId,
+    title: "style-redirect",
+    contexts: ["action"]
+  }, () => chrome.runtime.lastError);
+
+function updateStyleRedirect(value){
+  chrome.contextMenus.update(styleRedirectId, {title: styleRedirectData[value]["title"]});
+}
+updateStyleRedirect(styleRedirect);
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === styleRedirectId) {
+    if (styleRedirect == "old") {
+        styleRedirect = "boxy";
+    } else {
+        styleRedirect = "old";
+    }
+    updateStyleRedirect(styleRedirect);
+  }
+})
